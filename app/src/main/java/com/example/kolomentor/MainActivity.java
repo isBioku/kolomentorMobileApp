9d997248a53e178ec5e325533d83246a487f086d
bioku <bioku4real@gmail.com>
2022-05-13 21:51:22 +0100

    sign up page completed

--- app/src/main/java/com/example/kolomentor/MainActivity.java
+++ app/src/main/java/com/example/kolomentor/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sign_up);
 
     }
 }
