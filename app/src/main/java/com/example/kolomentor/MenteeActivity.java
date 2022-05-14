a903953b6b8064674f8f5913af6e4116149a71c3
bioku <bioku4real@gmail.com>
2022-05-14 02:21:43 +0100

    first draft of the mentee activity

---
+++ app/src/main/java/com/example/kolomentor/MenteeActivity.java
@@ -0,0 +1,14 @@
+package com.example.kolomentor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenteeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mentee_activity);
+    }
+}
