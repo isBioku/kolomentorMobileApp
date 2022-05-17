76c4d5aed05438131dadbe0846ec1a611a9e330a
bioku <bioku4real@gmail.com>
2022-05-17 22:53:53 +0100

    started designing the message activity

--- app/src/main/java/com/example/kolomentor/login.java
+++ app/src/main/java/com/example/kolomentor/login.java
@@ -19,6 +19,6 @@ public class login extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.career_selection);
+        setContentView(R.layout.activity_messages);
     }
 }
