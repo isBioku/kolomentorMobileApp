dbd209a08465a2e5c72d1a9b514596b407e8ca1b
bioku <bioku4real@gmail.com>
2022-05-19 16:09:05 +0100

    Designed the mentor's selection's activity, redesigned the search bar, added effect to the popup bar

--- app/src/main/java/com/example/kolomentor/CareerSelection.java
+++ app/src/main/java/com/example/kolomentor/CareerSelection.java
@@ -1,17 +1,29 @@
 package com.example.kolomentor;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import java.util.Objects;
 
 public class CareerSelection extends AppCompatActivity {
 
+
+    CardView appDevelopmentCareer;
+    ImageView svgAppDevCareer;
+
+    Dialog myDialog;
+    ImageView closeBtn;
+    LinearLayout cyber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +32,62 @@ public class CareerSelection extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.career_selection);
+        myDialog = new Dialog(this);
+
+
+
+
     }
 
-    public void popUpCard (View view) {
-        PopupMenu popupMenu = new PopupMenu(this, view);
-        popupMenu.inflate(R.menu.my_drawer_menu);
-        popupMenu.show();
+
+
+
+    public void goToAppDevelopmentMentors (View view ) {
+        appDevelopmentCareer = findViewById(R.id.appDevelopment);
+        svgAppDevCareer = findViewById(R.id.appDevelopmentSVG);
+        svgAppDevCareer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CareerSelection.this, SelectMentor.class));
+            }
+        });
+        appDevelopmentCareer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CareerSelection.this, SelectMentor.class));
+
+            }
+        });
     }
+//    public void popUpCard (View view) {
+//        PopupMenu popupMenu = new PopupMenu(this, view);
+//        popupMenu.inflate(R.menu.my_drawer_menu);
+//        popupMenu.show();
+//    }
+
+
+
+    public void popUpCard (View view) {
+
+        myDialog.setContentView(R.layout.activity_categories_popup);
+        closeBtn = myDialog.findViewById(R.id.closePopUp);
+        cyber = myDialog.findViewById(R.id.cyberSecurity);
+        closeBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                myDialog.dismiss();
+            }
+        });
+
+        cyber.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                myDialog.dismiss();
+            }
+        });
+
+        myDialog.show();
+    }
+
+
 }
---
+++ app/src/main/java/com/example/kolomentor/SelectMentor.java
@@ -0,0 +1,22 @@
+package com.example.kolomentor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Objects;
+
+public class SelectMentor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_select_mentor);
+    }
+}
